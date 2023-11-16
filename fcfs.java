fec0d8b9d0fa2bfb5b4f6608419396336eeb6a85
JheneJin <80924562+JheneJin@users.noreply.github.com>
2023-11-16 15:34:24 -0800

    Update fcfs.java
    ADD COMMENTS

--- fcfs.java
+++ fcfs.java
@@ -7,7 +7,7 @@ import java.util.*;
 public class FCFS implements Algorithm { 
     CPU cpu;
     LinkedList<Task> queue;
-
+    //FCFS Constructor
     public FCFS (List<Task> queue) {
         this.cpu = new CPU();
         this.queue = new LinkedList<>(queue);
@@ -16,20 +16,26 @@ public class FCFS implements Algorithm {
     public void schedule() {
         Task currentTask;
         int burstTime; 
+        //initalize queue size
         int originalQueueSize = queue.size();
 
 
         for (int i=0; i < originalQueueSize; i++){
+            //picks the next task as the current task
             currentTask = pickNextTask();
+            //gets burst time of the task
             burstTime = currentTask.getBurst();
 
+            //execute the current task using CPU
             cpu.run(currentTask, burstTime);
 
+            //remove the task from queue after its completion
             Task finishTask = queue.removeFirst();
             System.out.println("Task " + finishTask.getName() + " finished\n");
         }
     }
-
+ 
+    //gets the next task from the queue
     public Task pickNextTask() {
         Task task = queue.get(0);
         return task;
